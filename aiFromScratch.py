35a23cf7d36323b09ce09e6997a47ce94dff5695
WMYM <81931984+WashingtonYandun@users.noreply.github.com>
2021-09-21 23:37:15 -0500

    Improving the plot

--- aiFromScratch.py
+++ aiFromScratch.py
@@ -7,9 +7,10 @@ from sklearn.datasets import make_circles
 samples = 500
 features = 2  #bidimensional
 
-
 #plot
 X, Y = make_circles(n_samples = samples,factor=0.5,noise=0.06)
 print(Y)
-plt.scatter(X[:,0], X[:,1])
+plt.scatter(X[Y == 0, 0], X[Y == 0, 1], c= '#6a040f')
+plt.scatter(X[Y == 1, 0], X[Y == 1, 1], c= '#0a9396')
+plt.axis("equal")
 plt.show()
