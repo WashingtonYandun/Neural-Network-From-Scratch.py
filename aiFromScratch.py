f333d8230f7402274f429d05813b6adca1405f81
WMYM <81931984+WashingtonYandun@users.noreply.github.com>
2021-09-21 23:28:09 -0500

    See the problem we are going to solve (plot)

--- aiFromScratch.py
+++ aiFromScratch.py
@@ -2,3 +2,14 @@ import matplotlib.pyplot as plt
 import numpy as np 
 import scipy as sc
 from sklearn.datasets import make_circles
+
+#make datasets
+samples = 500
+features = 2  #bidimensional
+
+
+#plot
+X, Y = make_circles(n_samples = samples,factor=0.5,noise=0.06)
+print(Y)
+plt.scatter(X[:,0], X[:,1])
+plt.show()
