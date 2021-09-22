8bacb520c5d1491ffaee15653e411fb190e2c081
WMYM <81931984+WashingtonYandun@users.noreply.github.com>
2021-09-22 00:31:33 -0500

    finish the layer class

--- Layer.py
+++ Layer.py
@@ -1,3 +1,5 @@
 class Layer():
-    def __init__():
-        #
+    def __init__(self, connections, neurons, activationFunction):
+        self.activationFunction = activationFunction
+        self.w = np.random.rand(connections, neurons) * 2 - 1
+        self.b = np.random.rand(1, neurons) * 2 - 1
