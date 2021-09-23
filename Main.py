8869a793d7915ff6a8b04c996cb0cb7d7cfab5d7
WMYM <81931984+WashingtonYandun@users.noreply.github.com>
2021-09-23 17:19:47 -0500

    update main.py

--- Main.py
+++ Main.py
@@ -15,7 +15,3 @@ plt.scatter(X[Y == 0, 0], X[Y == 0, 1], c='#6a040f')
 plt.scatter(X[Y == 1, 0], X[Y == 1, 1], c='#0a9396')
 plt.axis("equal")
 plt.show()
-
-# xd = np.linspace(-5, 5, 10)
-# print(relu(xd))
-# plt.plot(xd, binaryStep(xd))
