0e920c64f4f1fc2d17532040a1b58b4e96c9da3b
WMYM <81931984+WashingtonYandun@users.noreply.github.com>
2021-10-12 11:00:29 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -10,20 +10,20 @@ samples = 500
 features = 2  # bi dimensional
 structure = [features, 4, 4, 4, 1]
 
-# plot the problem
-# X, Y = make_circles(n_samples=samples, factor=0.5, noise=0.06)
-# print(Y)
-# plt.scatter(X[Y == 0, 0], X[Y == 0, 1], c='#6a040f')
-# plt.scatter(X[Y == 1, 0], X[Y == 1, 1], c='#0a9396')
-# plt.axis("equal")
-# plt.show()
+# plot problem
+X, Y = make_circles(n_samples=samples, factor=0.5, noise=0.06)
+print(Y)
+plt.scatter(X[Y == 0, 0], X[Y == 0, 1], c='#6a040f')
+plt.scatter(X[Y == 1, 0], X[Y == 1, 1], c='#0a9396')
+plt.axis("equal")
+plt.show()
 
 
-# def createNeuralNetwork(structure, activationFunction):
-#     neuralNetwork = []
-#     for l, layer in enumerate(structure[:-1]):
-#         neuralNetwork.append(Layer(structure[l], structure[l+1], activationFunction))
-#     return neuralNetwork
-#
-#
-# print(createNeuralNetwork(structure, sigmoid))
+def createNeuralNetwork(structure, activationFunction):
+    neuralNetwork = []
+    for l, layer in enumerate(structure[:-1]):
+        neuralNetwork.append(Layer(structure[l], structure[l+1], activationFunction))
+    return neuralNetwork
+
+
+print(createNeuralNetwork(structure, sigmoid))
