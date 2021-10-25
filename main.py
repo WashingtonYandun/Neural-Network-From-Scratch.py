fa531d2471600759bec0e7a1e8e1d33ba3d1f6fa
WMYM <81931984+WashingtonYandun@users.noreply.github.com>
2021-10-24 21:13:42 -0500

    edit main

--- main.py
+++ main.py
@@ -3,14 +3,13 @@ import numpy as np
 import scipy as sc
 from sklearn.datasets import make_circles
 from function import *
-from Layer import *
+from layer import *
 
 # make datasets
 samples = 500
 features = 2  # bi dimensional
-structure = [features, 4, 4, 4, 1]
 
-# plot problem
+# plot the problem
 X, Y = make_circles(n_samples=samples, factor=0.5, noise=0.06)
 print(Y)
 plt.scatter(X[Y == 0, 0], X[Y == 0, 1], c='#6a040f')
@@ -19,17 +18,15 @@ plt.axis("equal")
 plt.show()
 
 
-def createNeuralNetwork(structure, activationFunction):
+# def a topology
+networkStructure = [features, 2, 4, 8, 4, 2, 1]
+
+
+def create_neural_network(structure, neuralNetworkFunction):
     neuralNetwork = []
     for l, layer in enumerate(structure[:-1]):
-        neuralNetwork.append(Layer(structure[l], structure[l+1], activationFunction))
+        neuralNetwork.append(Layer(structure[l], structure[l + 1], neuralNetworkFunction))
     return neuralNetwork
 
 
-print(createNeuralNetwork(structure, sigmoid))
 
-
-if __name__ == "__main__":
-    # pep 8 practice
-else:
-    # pep 8 practice
