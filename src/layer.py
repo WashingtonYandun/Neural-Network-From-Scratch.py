b42e57f01f592de5a38f95e21de0700afc03ab44
WashingtonYandun <washington.yandun06@gmail.com>
2022-06-21 21:03:53 -0500

    change the way i am implementing it

--- src/layer.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-
-class Layer:
-    def __init__(self, connections, neurons, activationFunction):
-        self.activationFunction = activationFunction
-        self.w = np.random.rand(connections, neurons) * 2 - 1
-        self.b = np.random.rand(1, neurons) * 2 - 1
