8cc7d4bcf89e193ec24be16b4691b13f06aca4c4
Washington Yandun <81931984+WashingtonYandun@users.noreply.github.com>
2023-08-17 22:15:57 -0500

    feat: Unify all in one file

--- src/main.py
+++ src/main.py
@@ -1,16 +1,70 @@
+# Libraries
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_circles
 
+# Activation functions
+def sigmoid(x):
+    """sgmoid dependent value from value, using numpy"""
+    return 1 / (1 + np.exp(-x))
+
+
+def binary_step(x):
+    """0 if value is negative, 1 if it is positive or 0, using numpy"""
+    return np.heaviside(x, 1)
+
+
+def tanh(x):
+    """hyperbolic tangent dependent value from value, using numpy"""
+    return np.tanh(x)
+
+
+def relu(x):
+    """0 if the value is negative, return x if its not, using numpy"""
+    return np.maximum(0, x)
+
+
+def relu_classic(x):
+    """0 if the value is negative, return x if its not, no libraries"""
+    xA = []
+    for i in x:
+        if i < 0:
+            xA.append(0)
+        else:
+            xA.append(i)
+    return xA
+
+
+def sigmoid_derivative(x):
+    """sigmoid derivative dependent value from value, using numpy"""
+    return sigmoid(x) * (1 - sigmoid(x))
+
+
+def tanh_derivative(x):
+    """hyperbolic tangent derivative dependent value from value, using numpy"""
+    return 1 - tanh(x) * tanh(x)
+
+
+def mse(yt, yf):
+    """mean square error cost function"""
+    return np.square(yf - yt).mean() 
+
+
+def mse_derivative(yt, yf):
+    """mean square error derivative cost function"""
+    return (yf - yt) / yt.size
+
+# Neural Layer class
 class NeuralLayer:
     def __init__(self, num_inputs, num_neurons, activation_function):
         self.activation_function = activation_function
         self.bias = np.random.rand(1, num_neurons) * 2 - 1
         self.weights = np.random.rand(num_inputs, num_neurons) * 2 - 1
 
+
 # Create the dataset
 num_samples = 1000
-num_features = 2
+num_features = 2 # 2D x and y
 
 X, y = make_circles(n_samples=num_samples, factor=0.6, noise=0.07)
 
@@ -18,3 +72,15 @@ plt.scatter(X[y == 0, 0], X[y == 0, 1], c="#1d3557")
 plt.scatter(X[y == 1, 0], X[y == 1, 1], c="#e63946") 
 plt.axis("equal")
 plt.show()
+
+# Create the model based on the topology
+topology = [num_features, 4, 8, 1]
+
+def create_nn(topology, activation_function):
+    nn = []
+    for l, layer in enumerate(topology[:-1]):
+        nn.append(NeuralLayer(topology[l], topology[l+1], activation_function))
+    return nn
+
+
+# Train the model
